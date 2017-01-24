e129f2bec8c89aadbe055162d775610e517adc66
talk2nadji <talk2nadji@hotmail.com>
2017-01-24 18:47:01 +0100

    products index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+product01 = Product.create!( {name: "Jam01", description: "Yummie!, nice jam!", price: 2.45} )
+product01.save
+product02 = Product.create!( {name: "Jam02", description: "This one is even nicer.., nice jam!", price: 3.45} )
+product02.save
+product03 = Product.create!( {name: "Jam03", description: "Great!, nice jam!", price: 4.45} )
+product03.save
