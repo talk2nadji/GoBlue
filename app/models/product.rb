676b2aa4723d9585245bc0c4596bd04674fcd268
talk2nadji <talk2nadji@hotmail.com>
2017-01-26 16:47:01 +0100

    create CRUD product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  validates :category_id, presence: true
 end
