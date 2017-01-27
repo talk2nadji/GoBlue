46fada1a4a4339ea65184f3f435ccc31875e57f6
talk2nadji <talk2nadji@hotmail.com>
2017-01-27 03:37:05 +0100

    add cart button with badge

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :check_user, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.all
